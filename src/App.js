c624fa0673ec66bb6d869d39f1dee33b3cf4174f
Wael Elsafty <waelelsafty07@gmail.com>
2023-06-16 11:28:01 +0300

    add: provider to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import { Provider } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import store from './redux/store';
 import Categories from './routes/Categories';
 import HomeRoute from './routes/Home';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomeRoute />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
+      <Provider store={store}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomeRoute />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Provider>
     </>
   );
 }
